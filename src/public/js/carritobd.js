abfe8729a23362784a1e4a8f6b781416238f09c9
jucamargo7 <juanpablo9911@hotmail.com>
2023-08-29 18:19:37 -0500

    Mejoras

--- src/public/js/carritobd.js
+++ src/public/js/carritobd.js
@@ -1,8 +1,9 @@
 function purcharse(){
     const idCarrito = document.getElementById("idCarrito").value
     axios.post(`/api/carritobd/${idCarrito}/purchase`).then((res)=>{
-        console.log(res)
+        alert("Se ha enviado un correo con la factura")
+        location.href="/productos"
     }).catch((error)=>{
-        console.log(error)
+        alert("Hubo un error en la compra")
     })  
 }
